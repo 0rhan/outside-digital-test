68f4721c903b089339d70d42d40d7c5111c47930
0rhan <gurbanov.orhan@vk.com>
2021-11-25 10:46:14 +0300

    feat(UI): BigButtonBase component

---
+++ src/UI/Components/Buttons/BigButton/BigButtonBase/BigButtonBase.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+import ButtonBase from "../../ButtonBase/ButtonBase";
+
+const BigButtonBase = styled(ButtonBase)`
+  height: 56px;
+  display: grid;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 24px;
+`;
+
+export default BigButtonBase;
